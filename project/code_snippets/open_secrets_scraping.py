f373eb6dbdcbe09fb77c2169e8f851ab1b5c22bd
Jacob-McLaughlin <108675286+Jacob-McLaughlin@users.noreply.github.com>
2022-12-01 19:33:24 +0000

    Create open_secrets_scraping.py

---
+++ project/code_snippets/open_secrets_scraping.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+import requests
+#Import the number of house districts for each state 
+congress_numbers = pd.read_csv("https://raw.githubusercontent.com/Jacob-McLaughlin/Jacob-McLaughlin.github.io/main/project/data/US_state_congress_numbers.csv")
+#Generate the codes for each district in the format "AL01"
+house_codes = []
+for i in range(len(congress_numbers)):
+  for j in range(1,congress_numbers.iloc[i][17]+1):
+    if j < 10:
+      short_code = congress_numbers.iloc[i][1] + "0" + str(j)
+    else:
+      short_code = congress_numbers.iloc[i][1] + str(j)
+    house_codes.append(short_code)
+url_base_year = "https://www.opensecrets.org/races/summary?cycle={}"
+url_base_house = "&id={}"
+#Select years to get results for 
+years = ["2020"]
+#Create empty dataframe to hold results
+house_finances = pd.DataFrame()
+for year in years:
+  url_year = url_base_year.format(year)
+  for code in house_codes:
+    url_house = url_base_house.format(code)
+    url = url_year + url_house
+    r = requests.get(url)
+    print(code)
+    secrets_page = pd.read_html(r.text)
+    for j in range(len(secrets_page[0])):
+      candidate = secrets_page[0].iloc[j][0]
+      if "Winner" in candidate: 
+        winner = "Y"
+      else:
+        winner = "N"
+      if "Incumbent" in candidate:
+        incumbent = "Y"
+      else:
+        incumbent = "N"
+      if "(R)" in candidate:
+        party = "gop"
+      elif "(D)" in candidate:
+        party = "dem"
+      short_name = candidate.split("(")[0]
+      amount_raised = secrets_page[0].iloc[j][1]
+      amount_spent = secrets_page[0].iloc[j][2]
+      row = pd.DataFrame([{'Seat': code, 'Candidate': short_name, 'Party': party, 'Raised': amount_raised, 'Spent': amount_spent, 'Winner': winner, 'Incumbent': incumbent}])
+      house_finances = pd.concat([house_finances, row]) 
+      house_finances.to_csv("2020_house_finances_open_secrets.csv")
